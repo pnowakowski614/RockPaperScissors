2953e3de1a7fe908f38a1aaec6a139a79c278753
pnowakowski614 <p.nowakowski416@gmail.com>
2021-07-28 01:56:05 +0200

    Added game function

--- rps-script.js
+++ rps-script.js
@@ -16,10 +16,40 @@ function capitalize(str) {
 }
 
 function RockPaperScissors(playerSelection, computerSelection) {
-    //
+    let computerMessage = "Computer chose: " + computerSelection + "\n\n";
+
+    let winMessage = computerMessage + "You won! " + playerSelection + " beats " + computerSelection;
+    let loseMessage = computerMessage + "You lost! " + computerSelection + " beats " + playerSelection;
+    let tieMessage = computerMessage + "It's a tie! You both chose " + playerSelection;
+
+    if (playerSelection === computerSelection) {
+        return tieMessage;
+    }
+    else if (playerSelection === "Rock") {
+        if (computerSelection === "Scissors") return winMessage;
+        else return loseMessage;
+    }
+    else if (playerSelection === "Scissors") {
+        if (computerSelection === "Paper") return winMessage;
+        else return loseMessage;
+    }
+    else {
+        if (computerSelection === "Rock") return winMessage;
+        else return loseMessage;
+    }
+
 }
 
+function game() {
+    for(let i = 0; i < 5; i++) {
 
-//let playerSelection = prompt("Make your choice! Rock, Paper or Scissors?");
+        let playerSelection = capitalize(prompt("Make your choice! Rock, Paper or Scissors?"));
 
-//RockPaperScissors(capitalize(playerSelection), computerPlay());
+        while((playerSelection !== "Rock") && (playerSelection !== "Paper") && (playerSelection !== "Scissors"))  {
+        playerSelection = capitalize(prompt("Wrong choice! Rock, Paper or Scissors?"));
+        } 
+        console.log(RockPaperScissors(playerSelection, computerPlay()));
+    }
+}
+
+game();
