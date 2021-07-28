2573bb6cf047a9b52fb6045cd09828107508b0b0
pnowakowski614 <p.nowakowski416@gmail.com>
2021-07-28 19:27:03 +0200

    Added score-keeping and winner announcement

--- rps-script.js
+++ rps-script.js
@@ -17,39 +17,68 @@ function capitalize(str) {
 
 function RockPaperScissors(playerSelection, computerSelection) {
     let computerMessage = "Computer chose: " + computerSelection + "\n\n";
-
     let winMessage = computerMessage + "You won! " + playerSelection + " beats " + computerSelection;
     let loseMessage = computerMessage + "You lost! " + computerSelection + " beats " + playerSelection;
     let tieMessage = computerMessage + "It's a tie! You both chose " + playerSelection;
 
     if (playerSelection === computerSelection) {
-        return tieMessage;
+        console.log(tieMessage);
+        return 0;
     }
     else if (playerSelection === "Rock") {
-        if (computerSelection === "Scissors") return winMessage;
-        else return loseMessage;
+        if (computerSelection === "Scissors") {
+            console.log(winMessage);
+            return 1;
+        }
+        else {
+            console.log(loseMessage);
+            return -1;
+        }
     }
     else if (playerSelection === "Scissors") {
-        if (computerSelection === "Paper") return winMessage;
-        else return loseMessage;
+        if (computerSelection === "Paper") {
+            console.log(winMessage);
+            return 1;
+        }
+        else {
+            console.log(loseMessage);
+            return -1;
+        }
     }
     else {
-        if (computerSelection === "Rock") return winMessage;
-        else return loseMessage;
+        if (computerSelection === "Rock") {
+            console.log(winMessage);
+            return 1;
+        }
+        else {
+            console.log(loseMessage)
+            return -1;
     }
-
+}
 }
 
 function game() {
-    for(let i = 0; i < 5; i++) {
 
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for(let i = 0; i < 5; i++) {
         let playerSelection = capitalize(prompt("Make your choice! Rock, Paper or Scissors?"));
 
         while((playerSelection !== "Rock") && (playerSelection !== "Paper") && (playerSelection !== "Scissors"))  {
         playerSelection = capitalize(prompt("Wrong choice! Rock, Paper or Scissors?"));
         } 
-        console.log(RockPaperScissors(playerSelection, computerPlay()));
+        let value = RockPaperScissors(playerSelection, computerPlay());
+
+        if (value === 1) ++playerScore;
+        if (value === -1) ++computerScore;
+
+        console.log("Current Score:\n" + "You: " + playerScore + "\nComputer: " + computerScore);
     }
-}
+
+    if(playerScore > computerScore) console.log("Congratulations! You've won " + playerScore + " to " + computerScore);
+    else if(playerScore < computerScore) console.log("Bad luck! You've lost " + playerScore + " to " + computerScore);
+    else console.log("The game is a tie! You've both won " + playerScore + " times");
+}   
 
 game();
